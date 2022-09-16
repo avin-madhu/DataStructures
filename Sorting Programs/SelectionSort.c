c235872ce898d90618167042333b5c76ace90f59
avin madhu <84233047+avin-madhu@users.noreply.github.com>
2022-09-16 23:56:23 +0700

    ss

--- Sorting Programs/SelectionSort.c
+++ Sorting Programs/SelectionSort.c
@@ -1,3 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int arr[100]={2,4,6,1,5,3},n=6;
+    int small,ind,temp=0,i,j;
 
-    
+    for(i=0;i<n-1;i++)
+    {
+      for(j=i;j<n-1;j++)
+      {
+        if(arr[j]<arr[i])
+        {
+            temp=arr[j];
+            arr[j]=arr[i];
+            arr[i]=temp;
+        }
+      }
+            
+    }
+
+
+for(int i=0;i<n;i++)
+{
+    printf("%d",arr[i]);
+}
+}    
     
