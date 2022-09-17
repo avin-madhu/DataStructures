5f77149066f1147706890f0f0c6d9344c1e3bec7
avin madhu <84233047+avin-madhu@users.noreply.github.com>
2022-09-17 16:22:01 +0700

    is

---
+++ Sorting Programs/InsertionSort.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+    int arr[100];
+    int n;
+
+    printf("Enter the number of elements: ");
+    scanf("%d",&n);
+    printf("\n");
+    printf("Enter the element: ");
+
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    printf("The sorted array is: ");
+    printf("\n");
+
+
+    for(int i=1;i<n;i++)
+    {
+      int current=arr[i];
+      int j=i-1;
+      while(arr[j]>current&&j>=0)
+      {
+        arr[j+1]=arr[j];
+        j--;
+      }
+      arr[j+1]=current;
+    }
+
+    for(int i=0;i<n;i++)
+    {
+       printf("%d ",arr[i]);
+    }
+    return 0;
+}
